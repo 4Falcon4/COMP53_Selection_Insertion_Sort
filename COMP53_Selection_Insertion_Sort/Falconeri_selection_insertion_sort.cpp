329e5db31d5670b33af7a60a16caf7286773476a
4falcon4 <falcon@4falcon4.com>
2024-09-10 15:54:30 -0700

    stuff

--- COMP53_Selection_Insertion_Sort/Falconeri_selection_insertion_sort.cpp
+++ COMP53_Selection_Insertion_Sort/Falconeri_selection_insertion_sort.cpp
@@ -5,86 +5,84 @@
 #include <chrono> 
 #include <ctime>
 using namespace std;
-void SelectionSort(vector<int>& v);
-void InsertionSort(vector<int>& v);
+void SelectionSort(vector<int>& v, int const size);
+void InsertionSort(vector<int>& v, int const size);
+void randomizer(vector<int>& v, int count);
 void PrintVector(vector<int> v);
 void PrintTime(chrono::high_resolution_clock::time_point start, chrono::high_resolution_clock::time_point end);
 
 int main()
 {
-	vector<int> selection{ 12, 23, 1, 54, 13, 43, 21, 38, 92, 8 };
-	vector<int> insertion{ 12, 23, 1, 54, 13, 43, 21, 38, 92, 8 };
+	srand(time(0));
+	int size = 1500;
+	vector<int> base;
+	randomizer(base, size);
 
-	PrintVector(selection);
-
+	vector<int> selection = base;
+	vector<int> insertion = base;
+	
 	auto startSelection = chrono::high_resolution_clock::now();
 
-	SelectionSort(selection);
+	SelectionSort(selection, size);
 
 	auto endSelection = chrono::high_resolution_clock::now();
-
+	
 	auto startInsertion = chrono::high_resolution_clock::now();
 
-	InsertionSort(insertion);
+	InsertionSort(insertion, size);
 
 	auto endInsertion = chrono::high_resolution_clock::now();
 
-	PrintVector(insertion);
 	PrintTime(startSelection, endSelection);
-
-	PrintVector(selection);
 	PrintTime(startInsertion, endInsertion);
 
 	return 0;
 }
 
-void SelectionSort(vector<int>& v)
+void SelectionSort(vector<int>& v, int const size)
 {
-	int size = v.size();
-
 	for (int i = 0; i < size; ++i)
 	{
-		int low = v.at(i);
-		int lowi = i;
+		int minIndex = i;
 
 		for (int j = i + 1; j < size; ++j)
 		{
-			if (v.at(j) < low)
+			if (v.at(j) < v.at(minIndex))
 			{
-				low = v.at(j);
-				lowi = j;
+				minIndex = j;
 			}
 		}
 
-		int temp = v.at(i);
-		v.at(i) = low;
-		v.at(lowi) = temp;
+		swap(v.at(i), v.at(minIndex));
 	}
 
 }
 
-void InsertionSort(vector<int>& v)
+void InsertionSort(vector<int>& v, int const size)
 {
-	int size = v.size();
-
 	for (int i = 1; i < size; ++i)
 	{
 		int key = v.at(i);
+		int j = i - 1;
 
-		for (int j = i - 1; j >= 0; --j)
+		while (j >= 0 && v.at(j) > key)
 		{
-			if (v.at(j) > key)
-			{
-				v.at(j + 1) = v.at(j);
-				v.at(j) = key;
-			}
-			else
-			{
-				break;
-			}
+			v.at(j + 1) = v.at(j);
+			j--;
 		}
+
+		v.at(j + 1) = key;
 	}
 
+}
+
+void randomizer(vector<int>& v, int count)
+{
+	v.clear();
+	for (int i = 0; i < count; i++)
+	{
+		v.push_back(rand() % 50);
+	}
 }
 
 void PrintVector(vector<int> v)
